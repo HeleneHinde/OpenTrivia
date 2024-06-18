f555c6a4289e8cd84de0c6b467bda6f313ca2def
HeleneHinde <hindepouet@gmail.com>
2024-06-18 10:33:48 +0200

    services

--- src/app/trivial/trivial.page.ts
+++ src/app/trivial/trivial.page.ts
@@ -2,38 +2,47 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Form, FormBuilder, FormGroup, FormsModule, Validators } from '@angular/forms';
 import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import {IonicModule} from '@ionic/angular';
 
 @Component({
   selector: 'app-trivial',
   templateUrl: './trivial.page.html',
   styleUrls: ['./trivial.page.scss'],
   standalone: true,
-  imports: [FormsModule, CommonModule, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [FormsModule, CommonModule, IonicModule]
 })
 export class TrivialPage implements OnInit {
   pseudo: string = "";
   difficulties: string[] = ['easy', 'medium', 'hard'];
-  selectedDifficulty: string = 'easy';
+  selectedDifficulty: string = this.difficulties[0];
   saveInfo: boolean = false;
   gameStarted: boolean = false;
   answer: boolean = false;
+  isToast: boolean = false;
 
   constructor() { }
 
   ngOnInit() { }
 
   pseudoInvalid() {
-    return this.pseudo.length > 0 && this.pseudo.length < 3;
+
+    if(this.pseudo.length > 0 && this.pseudo.length < 3)
+      this.isToast=true;
+    else
+    this.isToast=false;
   }
 
   startGame() {
     if (this.pseudo.length >= 3) {
       this.gameStarted = true;
+      this.isToast=false;
       // Logique supplémentaire pour sauvegarder les informations si nécessaire
       if (this.saveInfo) {
         localStorage.setItem('pseudo', this.pseudo);
         localStorage.setItem('difficulty', this.selectedDifficulty);
       }
+    } else {
+      this.isToast=true;
     }
   }
 
